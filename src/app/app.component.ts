9fd51fa3a52e005ead98a4b4bb441850564dae10
Raito Bezarius <masterancpp@gmail.com>
2022-02-11 17:52:07 +0100

    milestone: poc v1 with tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,157 @@
 import { Component, VERSION } from '@angular/core';
 import PSPDFKit from 'pspdfkit';
 
-let counter = 0;
+const curTab = {
+  uri: "/assets/dummy.pdf",
+  title: "PDF 01"
+};
 
-const tabs = [
-  { tab: '01.pdf' },
-  { tab: '02.pdf' },
-  { tab: '03.pdf' },
-];
+const allTabs = {
+  "/assets/dummy.pdf": {
+    title: "PDF 01",
+    uri: "/assets/dummy.pdf"
+  }
+};
+
+const initialTabs = [ curTab ];
+type Tab = {
+  uri: string;
+};
+
+function loadPDF(uri, toolbarItems) {
+    return PSPDFKit.load({
+      baseUrl: location.protocol + "//" + location.host + "/assets/",
+      container: '#container',
+      document: uri,
+      styleSheets: ['/assets/styles.css'],
+      toolbarItems,
+    }).then((instance) => {
+      console.log('PSPDFKit loaded!');
+      (window as any).instance = instance;
+      return instance;
+    });
+}
+
+class TabService {
+  activeEditor = null;
+  activeInstance = null;
+  activeTab = 0;
+  openTabs = initialTabs;
+  toolbarItems = [];
+
+  constructor() {
+    const linkCurrentPageToolbarItem = {
+      type: ("custom" as any),
+      id: "link-current-page",
+      title: "Ajouter un lien vers cette page dans les notes",
+      onPress: evt => this.linkToCurrentPage()
+    };
+
+    this.toolbarItems.push(linkCurrentPageToolbarItem);
+  }
+
+  getCurrentTab() {
+    return this.openTabs[this.activeTab];
+  }
+
+  getCurrentPage() {
+    return this.activeInstance.viewState.currentPageIndex;
+  }
+
+  linkToCurrentDocument() {
+    this.insertLinkToEditor(this.getLinkFor({ tab: this.getCurrentTab() }));
+  }
+
+  linkToCurrentPage() {
+    this.insertLinkToEditor(this.getLinkFor({ tab: this.getCurrentTab(), page: this.getCurrentPage() }));
+  }
+
+  reloadPDF() {
+    const { uri } = this.getCurrentTab();
+    if (this.activeInstance) {
+      PSPDFKit.unload(this.activeInstance);
+    }
+    return loadPDF(uri, this.toolbarItems).then(instance => {
+      this.activeInstance = instance;
+    });
+  }
+
+  getLinkFor({ tab, page }: { tab: Tab, page?: string }) {
+    return {
+      internal: !!page ? `${tab.uri}@${page}` : tab.uri,
+      text: page != null ? `${tab.uri} (page ${page + 1})` : tab.uri
+    };
+  }
+
+  openNewTab(tab) {
+    this.openTabs.push(tab);
+    this.activeTab = this.openTabs.length - 1;
+    return this.reloadPDF();
+  }
+
+  locateExistingTab(uri) {
+    const targetTab = this.openTabs.findIndex(item => item.uri === uri);
+    if (targetTab === -1) {
+      return null;
+    } else {
+      return targetTab;
+    }
+  }
+
+  focusTab(tabIndex) {
+    this.activeTab = tabIndex;
+    return this.reloadPDF();
+  }
+
+  scrollToPage(pageIndex) {
+    this.activeInstance.setViewState(state => state.set("currentPageIndex", pageIndex));
+  }
+
+  openInternalLink(link) {
+    let page = null;
+    let uri;
+    if (link.includes("@")) {
+      [uri, page] = link.split("@");
+    } else {
+      uri = link;
+    }
+
+    const tabId = this.locateExistingTab(uri);
+    let p = null
+    if (tabId != null) {
+      p = this.focusTab(tabId);
+    } else {
+      p = this.openNewTab(allTabs[uri]);
+    }
+
+    if (page != null) {
+      Promise.resolve(p).then(() => this.scrollToPage(+page));
+    } 
+  }
+
+  insertLinkToEditor(link) {
+    const editor = this.activeEditor;
+    // Plusieurs pistes ici:
+    // - un <button /> mais le styling TinyMCE est pas très joli
+    //   -> de plus, on perd les menus contextuels de manipulations des liens (remove link, open link, etc.)
+    // - un <a href="#">, a le bon styling, peut se faire changer par sélection puis reclic sur lier
+
+    if (!!editor.selection && !editor.selection.isCollapsed()) {
+      // Petit bug: sélection un coup, puis une seconde sélection et un lien par dessus contextuel
+      // cause une insertion d'un excès de noeuds, j'ai pas investigué pourquoi mais je peux si nécessaire
+      // Il faut renforcer la condition de la ligne 59 à mon avis
+      editor.insertContent(
+        `<a data-internal-link="${link.internal}" href="#">${
+          editor.selection.getNode().innerHTML
+        }</a>`
+      );
+    } else {
+      editor.insertContent(
+        `<a data-internal-link="${link.internal}" href="#">${link.text}</button>`
+      );
+    }
+  }
+}
 
 @Component({
   selector: 'my-app',
@@ -16,6 +160,7 @@ const tabs = [
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  tabService = new TabService();
   tinymceOptions = {
     height: 500,
     menubar: false,
@@ -29,48 +174,14 @@ export class AppComponent {
     //suffix: '.min', // Suffix to use when loading resources
   };
 
-  ngOnInit() {
-    PSPDFKit.load({
-      container: '#container',
-      document:
-        'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Hélas, ça ne charge pas le PDF :(
-      styleSheets: ['/assets/styles.css'],
-    }).then((instance) => {
-      console.log('PSPDFKit loaded!');
-      console.log('API docs: https://pspdfkit.com/api/web/');
-      console.log('Guides: https://pspdfkit.com/guides/web/');
-    });
+  ngAfterViewInit() {
+    this.tabService.reloadPDF();
+
+    const that = this;
 
     this.tinymceOptions['setup'] = function setup(editor: any) {
-      function get_current_tab() {
-        const cur_tab = tabs[counter % tabs.length];
-        counter += 1;
-        return cur_tab;
-      }
-
-      function link_current_doc(item) {
-        console.log('insérer le lien vers le tab', get_current_tab(), item);
-        console.log(editor.selection.getNode());
-        const { tab } = get_current_tab();
-        // Plusieurs pistes ici:
-        // - un <button /> mais le styling TinyMCE est pas très joli
-        //   -> de plus, on perd les menus contextuels de manipulations des liens (remove link, open link, etc.)
-        // - un <a href="#">, a le bon styling, peut se faire changer par sélection puis reclic sur lier
-        if (!!editor.selection && !editor.selection.isCollapsed()) {
-          // Petit bug: sélection un coup, puis une seconde sélection et un lien par dessus contextuel
-          // cause une insertion d'un excès de noeuds, j'ai pas investigué pourquoi mais je peux si nécessaire
-          // Il faut renforcer la condition de la ligne 59 à mon avis
-          editor.insertContent(
-            `<a data-internal-link="${tab}" href="#">${
-              editor.selection.getNode().innerHTML
-            }</a>`
-          );
-        } else {
-          editor.insertContent(
-            `<a data-internal-link="${tab}" href="#">${tab}</button>`
-          );
-        }
-      }
+      (window as any).editor = editor;
+      that.tabService.activeEditor = editor;
 
       // Menu contextuel lorsqu'on sélectionne un texte.
       editor.ui.registry.addContextToolbar('textselection', {
@@ -86,13 +197,17 @@ export class AppComponent {
       editor.ui.registry.addButton('customLinkButton', {
         icon: 'link',
         text: 'Lier le présent document',
-        onAction: link_current_doc,
+        onAction: () => that.tabService.linkToCurrentDocument(),
         onSetup: function (buttonApi) {
           const clickOnInternalLink = function (evtApi) {
             // On peut intercepter les clics vers les liens ici et cancel l'action au niveau navigateur.
             // En raison des contraintes de sécurité de Stackblitz, on peut pas bien voir le log de la console.
             // Il faut ouvrir le logging au niveau du navigateur et constater que les erreurs passent bien par la bonne stacktrace.
-            console.log('clicked', evtApi, this.target);
+            if (evtApi.target.hasAttribute("data-internal-link")) {
+              const internalLink = evtApi.target.getAttribute("data-internal-link");
+              console.log("clicked on an internal link to", internalLink);
+              that.tabService.openInternalLink(internalLink);
+            }
           };
           editor.on('click', clickOnInternalLink);
 
